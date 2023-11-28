e31d8d2111dc54b05046c0d4b1058f64cfc04ec2
Raghuveer k j <150252274+Raghunayak01@users.noreply.github.com>
2023-11-28 13:25:22 +0530

    Update Weather.py

--- Weather.py
+++ Weather.py
@@ -1,128 +1,61 @@
-from tkinter import *
-import tkinter as tk
-from geopy.geocoders import Nominatim
-from tkinter import ttk, messagebox
-from timezonefinder import TimezoneFinder
-from datetime import*
+from flask import Flask, jsonify, request
 import requests
-import pytz
-from PIL import Image, ImageTk
+from datetime import datetime
 
-root=Tk()
-root.title("Weather App By Raghuveer K J")
-root.geometry("900x500+300+200")
-root.resizable(False,False)
+app = Flask(_name_)
+api_key = "5e2b250925b6337db4d077da5a6f4ee5" #API key
 
-def getWeather():
+def kelvin_to_celsius(kelvin_temp):
+    return kelvin_temp - 273.15
+
+def format_date(timestamp):
+    return datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d')
+
+@app.route('/weather', methods=['GET'])
+def get_weather():
+    locations = request.args.getlist('location')  # Accept multiple locations as a list
+
+    if not locations:
+        return jsonify({'error': 'At least one location is required'}), 400
+
+    weather_data = []
+    for location in locations:
+        city, state = map(str.strip, location.split(','))
+        data = get_weather_data(city, state)
+        weather_data.append(data)
+
+    return jsonify(weather_data)
+
+def get_weather_data(city, state):
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'q': f'{city},{state}',
+        'appid': api_key,
+    }
+
     try:
-        
-        city=textfield.get()
+        response = requests.get(base_url, params=params)
+        response.raise_for_status()
+        data = response.json()
 
-        geolocator=Nominatim(user_agent="geoapiExercises")
-        location=geolocator.geocode(city)
-        obj=TimezoneFinder()
-        result=obj.timezone_at(lng=location.longitude,lat=location.latitude)
+        # Extract relevant information from the API response
+        weather_info = {
+            'city': data['name'],
+            'date': format_date(data['dt']),
+            'day': datetime.utcfromtimestamp(data['dt']).strftime('%A'),
+            'temperature': kelvin_to_celsius(data['main']['temp']),
+            'max_temperature': kelvin_to_celsius(data['main']['temp_max']),
+            'min_temperature': kelvin_to_celsius(data['main']['temp_min']),
+            'description': data['weather'][0]['description'],
+            'precipitation': data['weather'][0].get('main', 'N/A'),
+            'humidity': data['main'].get('humidity', 'N/A'),
+            'wind_speed': data['wind'].get('speed', 'N/A'),
+        }
 
-        home=pytz.timezone(result)
-        local_time=datetime.now(home)
-        current_time=local_time.strftime("%I:%M %p")
-        clock.config(text=current_time)
-        name.config(text="CURRENT WEATHER")
+        return weather_info
 
-        #weather
-        api="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=21afe8ccb1aadd2fdc0a53f19c400e8e"
+    except requests.exceptions.RequestException as e:
+        return {'error': f'Error fetching data from OpenWeatherMap: {str(e)}'}
 
-        json_data = requests.get(api).json()
-        condition = json_data['weather'][0]['main']
-        description = json_data['weather'][0]['description']
-        temp = int(json_data['main']['temp']-273.15)
-        pressure = json_data['main']['pressure']
-        humidity = json_data['main']['humidity']
-        wind = json_data['wind']['speed']
-
-        t.config(text=(temp,"°"))
-        c.config(text=(condition,"|","FEELS","LIKE",temp,"°"))
-
-        w.config(text=wind)
-        h.config(text=humidity)
-        d.config(text=description)
-        p.config(text=pressure)
-    except Exception as e:
-        messagebox.showerror("Weather App","Invalid Entry!!")
-
-##icon
-image_icon=PhotoImage(file="main.png")
-root.iconphoto(False,image_icon)                       
-
-#search box
-Search_image=PhotoImage(file="search.png")
-myimage=Label(image=Search_image)
-myimage.place(x=20,y=20)
-
-textfield=tk.Entry(root,justify="center",width=17,font=("poppins",25,"bold"),bg="#404040",border=0,fg="white")
-textfield.place(x=50,y=40)
-textfield.focus()
-
-Search_icon=PhotoImage(file="search_icon.png")
-myimage_icon=Button (image=Search_icon,borderwidth=0,cursor="hand2",bg="#404040",command=getWeather)
-myimage_icon.place(x=400,y=34)
-
-#logo
-Logo_image=PhotoImage(file="logo.png")
-logo=Label(image=Logo_image)
-logo.place(x=150,y=100)
-
-#Bottom box
-Frame_image=PhotoImage(file="box.png")
-frame_myimage=Label(image=Frame_image)
-frame_myimage.pack(padx=5,pady=5,side=BOTTOM)
-
-#time
-name=Label(root,font=("arial",15,"bold"))
-name.place(x=30,y=100)
-clock=Label(root, font=("Helvetica",20))
-clock.place(x=30,y=130)
-
-#label
-label1=Label(root,text="WIND",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
-label1.place(x=120,y=400)
-
-label2=Label(root,text="HUMIDITY",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
-label2.place(x=250,y=400)
-
-label3=Label(root,text="DESCRIPTION",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
-label3.place(x=430,y=400)
-
-label4=Label(root, text="PRESSURE",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
-label4.place(x=650,y=400)
-
-t=Label(font=("arial",70,"bold"),fg="#ee666d")
-t.place(x=400,y=150)
-c=Label(font=("arial",15,'bold'))
-c.place(x=400,y=250)
-
-w=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
-w.place(x=120,y=430)
-h=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
-h.place(x=280,y=430)
-d=Label(text="...",font=("arial", 20,"bold"),bg="#1ab5ef")
-d.place(x=450,y=430)
-p=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
-p.place(x=670,y=430)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if _name_ == '_main_':
+    app.run(debug=True)
